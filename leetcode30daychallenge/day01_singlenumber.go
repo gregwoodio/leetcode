fa8aff7fe85d0ade1ac28be46e88ae3e91ce36b8
Greg Wood <contact@gregwood.io>
2020-04-01 08:19:47 -0400

    Leetcode 30 day challenge day 1

---
+++ leetcode30daychallenge/day01_singlenumber.go
@@ -0,0 +1,47 @@
+package leetcode30daychallenge
+
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// Example 1:
+
+// Input: [2,2,1]
+// Output: 1
+
+// Example 2:
+
+// Input: [4,1,2,1,2]
+// Output: 4
+
+func singleNumber(nums []int) int {
+	myMap := make(map[int]bool)
+
+	for _, num := range nums {
+		_, ok := myMap[num]
+		if !ok {
+			myMap[num] = false
+		} else {
+			myMap[num] = true
+		}
+	}
+
+	for key, val := range myMap {
+		if val == false {
+			return key
+		}
+	}
+
+	return -1
+}
+
+// Better solution (not mine):
+// func singleNumber(nums []int) int {
+//     result:=0
+//     for _,v:= range nums{
+//        result^=v
+//     }
+//     return result
+// }
---
+++ leetcode30daychallenge/day01_singlenumber_test.go
@@ -0,0 +1,30 @@
+package leetcode30daychallenge
+
+import "testing"
+
+type testData struct {
+	input    []int
+	expected int
+}
+
+func TestSingleNumber(t *testing.T) {
+
+	testDatas := []testData{
+		testData{
+			input:    []int{2, 2, 1},
+			expected: 1,
+		},
+		testData{
+			input:    []int{4, 1, 2, 1, 2},
+			expected: 4,
+		},
+	}
+
+	for _, td := range testDatas {
+		actual := singleNumber(td.input)
+
+		if td.expected != actual {
+			t.Errorf("Expected %d but was %d", td.expected, actual)
+		}
+	}
+}
